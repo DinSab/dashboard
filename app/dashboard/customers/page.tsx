d9bac401135fc90a7287c90fa89cac8d981c3ae4
DinS <din.sabic@sgkb.ch>
2024-09-11 08:21:03 +0200

    Finished the Tutorial

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Customers",
+};
 
 const Customers = () => {
-  return (
-    <div>Customers Page</div>
-  )
-}
+  return <div>Customers Page</div>;
+};
 
-export default Customers
+export default Customers;
